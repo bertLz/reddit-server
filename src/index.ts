e61a274ebd3727ce92315b856cf61f715a6f0e51
bertLz <xdaegonx@gmail.com>
2021-10-04 01:36:29 +0000

    graphql init

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,34 @@
-import { MikroORM } from "@mikro-orm/core"
-import { Post } from "./entities/Post";
-import mikroConf from "./mikro-orm.config"
+import { MikroORM } from "@mikro-orm/core";
+import mikroConf from "./mikro-orm.config";
+import express from 'express';
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
 
 const main = async () => {
     const orm = await MikroORM.init(mikroConf);
     await orm.getMigrator().up();
-    const post = orm.em.create(Post, { title: 'my first post', last: "mcc" })
-    await orm.em.persistAndFlush(post)
 
-    const posts = await orm.em.find(Post, {});
-    console.log(posts);
+    const app = express();
 
-}
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver, UserResolver],
+            validate: false
+        }),
+        context: () => ({ em: orm.em })
+    })
+    await apolloServer.start()
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(4000, () => {
+        console.log("server started on Port 4000");
+
+    })
+};
 
 main().catch((err) => {
     console.error(err);
-
 })
